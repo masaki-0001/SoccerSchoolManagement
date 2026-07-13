54886e88991bbe6dcab99e107d1d0a02d2c497aa
masaki-0001 <masaki.fue@gmail.com>
2026-07-13 09:01:34 +0900

    プロジェクトファイルを追加。

---
+++ SoccerSchoolManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SoccerSchoolManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
